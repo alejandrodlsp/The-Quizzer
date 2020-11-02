6103993ca92ce49253f4ec3af137f6a130dfd4e5
Dylan Hughes <47777699+Dylanh765@users.noreply.github.com>
2020-11-02 10:10:48 +0000

    high scores
    added high scores class

---
+++ app/src/main/java/ie/ul/theriddler/highScores.java
@@ -0,0 +1,14 @@
+package ie.ul.theriddler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class highScores extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_high_scores);
+    }
+}
