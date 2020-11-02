b40e5a664ddefb688619b33f5b4ef2bc7d249c3d
Dylan Hughes <47777699+Dylanh765@users.noreply.github.com>
2020-11-02 10:23:09 +0000

    can go to high scores and back

--- app/src/main/java/ie/ul/theriddler/highScores.java
+++ app/src/main/java/ie/ul/theriddler/highScores.java
@@ -2,7 +2,10 @@ package ie.ul.theriddler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class highScores extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class highScores extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_high_scores);
+
+        Button main_hub_button = (Button) findViewById(R.id.main_hub_button);
+        main_hub_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity( new Intent (highScores.this, mainHub.class));
+            }
+        });
     }
 }
