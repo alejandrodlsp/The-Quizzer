d5ed5741d8f5f7b68daf2f6ec7e4519a9b9d4776
Dylan Hughes <47777699+Dylanh765@users.noreply.github.com>
2020-12-07 12:09:08 +0000

    Added Highscore functionality

--- app/src/main/java/ie/ul/theriddler/layout/hub/HighScoresActivity.java
+++ app/src/main/java/ie/ul/theriddler/layout/hub/HighScoresActivity.java
@@ -6,10 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import ie.ul.theriddler.R;
+import ie.ul.theriddler.database.DatabaseHandler;
+import ie.ul.theriddler.questions.Question;
 
 public class HighScoresActivity extends AppCompatActivity {
+
+    TextView highscoresScores = (TextView) findViewById(R.id.highscoresScores);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +28,16 @@ public class HighScoresActivity extends AppCompatActivity {
                 startActivity( new Intent (HighScoresActivity.this, MainHubActivity.class));
             }
         });
+
+        //Looping for each category high score
+        String builder = "";
+        for(final Question.Category category : Question.Category.values()) {
+            builder += category.ToString() + ":";
+            int highscore = DatabaseHandler.GetInstance().GetCategoryHighscore(category);
+            builder += highscore + "\n";
+        }
+
+        highscoresScores.setText(builder);
+
     }
 }
